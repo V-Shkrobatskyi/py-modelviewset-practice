dde9d48a08ab55f4a14b1fc85f3d7facc79b8eca
Vitalii <vetali5700gmail.com>
2024-05-15 23:11:12 +0300

    fix flake8 errors

--- author/serializers.py
+++ author/serializers.py
@@ -4,7 +4,7 @@ from author.models import Author
 
 
 class AuthorSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
+    id_ = serializers.IntegerField(read_only=True)
     first_name = serializers.CharField(max_length=64)
     last_name = serializers.CharField(max_length=64)
     pseudonym = serializers.CharField(required=False, max_length=64)
